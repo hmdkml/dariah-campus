a9c8a11249367dd9f8e5b3cd094565e17e379a1e
Stefan Probst <stefanprobst@posteo.de>
2024-08-01 15:20:33 +0200

    chore: allow custom heading ids

--- config/mdx.config.js
+++ config/mdx.config.js
@@ -8,6 +8,7 @@ import withGfm from "remark-gfm";
 import withMdxFrontmatter from "remark-mdx-frontmatter";
 import withTypographicQuotes from "remark-smartypants";
 
+import { withCustomHeadingIds } from "../lib/content/with-custom-heading-ids.js";
 import { config as syntaxHighlighterConfig } from "./syntax-highlighter.config.js";
 
 /** @type {(locale: Locale) => Promise<CompileOptions>} */
@@ -15,7 +16,11 @@ export async function createMdxConfig(_locale) {
 	/** @type {CompileOptions} */
 	const config = {
 		remarkPlugins: [withFrontmatter, withMdxFrontmatter, withGfm, withTypographicQuotes],
-		rehypePlugins: [withHeadingIds, [withSyntaxHighlighter, syntaxHighlighterConfig]],
+		rehypePlugins: [
+			withCustomHeadingIds,
+			withHeadingIds,
+			[withSyntaxHighlighter, syntaxHighlighterConfig],
+		],
 	};
 
 	return Promise.resolve(config);
