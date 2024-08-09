bc0dcc99ea8c76445818ab4dd370e67dc10abd71
Stefan Probst <stefanprobst@posteo.de>
2024-08-09 17:42:13 +0200

    fix: disable twitter validation

--- lib/content/validation.ts
+++ lib/content/validation.ts
@@ -1,3 +1,13 @@
-export const mastodonHandle = { regex: /^@.+/, message: "Must start with '@' character." };
+export const mastodonHandle = {
+	/** FIXME: @see https://github.com/Thinkmill/keystatic/issues/1261 */
+	// regex: /^@.+/,
+	regex: undefined,
+	message: "Must start with '@' character.",
+};
 
-export const twitterHandle = { regex: /^@.+/, message: "Must start with '@' character." };
+export const twitterHandle = {
+	/** FIXME: @see https://github.com/Thinkmill/keystatic/issues/1261 */
+	// regex: /^@.+/,
+	regex: undefined,
+	message: "Must start with '@' character.",
+};
